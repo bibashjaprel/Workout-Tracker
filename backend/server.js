875f894ac7bbe7400a7a75f783d7896581b70bb1
Bibash Japrel <bibashjaprel111@gmail.com>
2024-04-07 13:20:44 +0545

    remove firebase config

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,6 @@ const PORT = process.env.PORT;
 const mongoose = require('mongoose')
 const requireAuth = require('./middleware/requireAuth')
 
-//firebase config
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDSEtsz6UtBV9qAqnB99TdWLKDp_mxMNDQ",
-  authDomain: "backend-workout.firebaseapp.com",
-  projectId: "backend-workout",
-  storageBucket: "backend-workout.appspot.com",
-  messagingSenderId: "942750784750",
-  appId: "1:942750784750:web:e1e2cac2a02ea0b39567ff",
-  measurementId: "G-KT116WRNR5"
-};
-
 // Initialize Firebase
 const apps = initializeApp(firebaseConfig);
 const analytics = getAnalytics(apps);
